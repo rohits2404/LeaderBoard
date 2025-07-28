0d59bcf4eadb3cf83e98dd371a703f93196463a0
Rohit Sharma <rohits8731@gmail.com>
2025-07-28 20:54:28 +0530

    02: MongoDB Connection

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/board`);
+        console.log("MongoDB Connected!");
+    } catch (error) {
+        console.log("MongoDB Connection Failed: ",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
